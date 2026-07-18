8e1dea65efd66112d840d6ce2b25a11db2311b89
Yagyaraj <yagyaraj.lodhi@wavemaker.com>
2026-07-18 17:36:25 +0530

    fix: stop extension hydration error loop

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -36,11 +36,11 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<HeadContent />
 			</head>
-			<body>
+			<body suppressHydrationWarning>
 				{children}
 				<Scripts />
 			</body>
